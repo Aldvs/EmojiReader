1ff4908d9b8b9d8f187afbb8d6aa48fc49f2d280
aldvs <andrewgogol777@gmail.com>
2022-01-25 22:20:22 +0300

    Setup EmojiTableViewCell with additional EmojiTableViewCell.swift file and modify EmojiTableViewController

---
+++ EmojiReader/ViewControllers/EmojiTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiReader
+//
+//  Created by admin on 25.01.2022.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
