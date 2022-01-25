84049a59622f61607239a8080a4a4489b1f180b2
aldvs <andrewgogol777@gmail.com>
2022-01-25 22:42:18 +0300

    Setup EmojiCell with new objects

---
+++ EmojiReader/Model/EmojiModel.swift
@@ -0,0 +1,15 @@
+//
+//  DataManager.swift
+//  EmojiReader
+//
+//  Created by admin on 25.01.2022.
+//
+
+import Foundation
+
+struct Emoji {
+    var emoji: String
+    var name: String
+    var description: String
+    var isFavorite: Bool
+}
