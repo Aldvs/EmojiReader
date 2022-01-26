5e821df0458c2a656eb87e3d9e5f2fac1c0b514d
aldvs <andrewgogol777@gmail.com>
2022-01-26 09:48:48 +0300

    Setup NewEmojiTableViewController

---
+++ EmojiReader/ViewControllers/NewEmojiTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  NewEmojiTableViewController.swift
+//  EmojiReader
+//
+//  Created by admin on 26.01.2022.
+//
+
+import UIKit
+
+class NewEmojiTableViewController: UITableViewController {
+
+    @IBOutlet weak var emojiTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func textChanged(_ sender: UITextField) {
+    }
+    
+    // MARK: - Table view data source
+
+}
